8390f868e2c03dabb03bc545efa7f005b14aa716
naveen kumar <lnaveenkumar123.nk@gmail.com>
2025-03-10 11:18:29 +0530

    Bugs fixed

--- Wassandashboard/Migrations/20250310053541_addedregionid.Designer.cs
+++ Wassandashboard/Migrations/20250310053541_addedregionid.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Wassandashboard.Data;
 
@@ -11,9 +12,11 @@ using Wassandashboard.Data;
 namespace Wassandashboard.Migrations
 {
     [DbContext(typeof(DashboardDbContext))]
-    partial class DashboardDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250310053541_addedregionid")]
+    partial class addedregionid
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Wassandashboard/Migrations/20250310053541_addedregionid.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Wassandashboard.Migrations
+{
+    /// <inheritdoc />
+    public partial class addedregionid : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProjectRegions_projects_ProjectId",
+                table: "ProjectRegions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ProjectRegions_ProjectId",
+                table: "ProjectRegions");
+
+            migrationBuilder.AddColumn<long>(
+                name: "RegionsId",
+                table: "ProjectRegions",
+                type: "bigint",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProjectRegions_RegionsId",
+                table: "ProjectRegions",
+                column: "RegionsId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProjectRegions_projects_RegionsId",
+                table: "ProjectRegions",
+                column: "RegionsId",
+                principalTable: "projects",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProjectRegions_projects_RegionsId",
+                table: "ProjectRegions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ProjectRegions_RegionsId",
+                table: "ProjectRegions");
+
+            migrationBuilder.DropColumn(
+                name: "RegionsId",
+                table: "ProjectRegions");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProjectRegions_ProjectId",
+                table: "ProjectRegions",
+                column: "ProjectId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProjectRegions_projects_ProjectId",
+                table: "ProjectRegions",
+                column: "ProjectId",
+                principalTable: "projects",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
